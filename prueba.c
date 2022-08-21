9da776e3bce30b5f8bf5bc036cf929502cdf645b
Pedro Sebastian Degaudenci Ferreira <pdegaude@c1r17s2.42madrid.com>
2022-08-21 18:03:11 +0200

    Prueba de lectura con archivos basicos hecho

--- prueba.c
+++ prueba.c
@@ -5,7 +5,7 @@
 #include <fcntl.h>
 int main(void)
 {
-    int fd = open("41_with_nl",O_RDONLY);
+    int fd = open("./files/example1",O_RDONLY);
    printf("%s",get_next_line(fd));
 printf("%s",get_next_line(fd));
     //system("leaks a.out");
