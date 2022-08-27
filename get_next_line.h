7bf570f2ac1b3a8ae08482bdab88f6f839bbdf26
Pedro Sebastian Degaudenci Ferreira <pdegaude@c1r17s4.42madrid.com>
2022-08-27 21:17:47 +0200

    definida constantes BUFFER_SIZE

--- get_next_line.h
+++ get_next_line.h
@@ -16,6 +16,10 @@
 # include <stdlib.h>
 # include <unistd.h>
 
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+# endif
+
 char	*get_next_line(int fd);
 char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strchr(const char *string, int searchedChar );
